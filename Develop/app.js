84a027c016ea7558ae50a11dffb4a361f5828e67
Tria Thao <triathao@gmail.com>
2020-12-12 17:27:49 -0800

    appjs code

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,45 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+createManager()
+
+let employees = []
+
+
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's ID?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email address?"
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is your manager's office number?"
+            },
+        ])
+        .then(response => {
+            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOffice)
+            employees.push(manager)
+            nextStep()
+            
+        })
+}
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Employee Summary - Team Builder
 
 ## About
-
 This application will generates an HTML file showing the Employees on a teamby inputing information into the terminal. Created using test driven development, JavaScript, Node.js, Jest, and Inquirer.
 
 ## Usage
-
 The user will be ask questions from prompt.
+
+## Usage
+Enter the following command into the command line to test: npm test
+
